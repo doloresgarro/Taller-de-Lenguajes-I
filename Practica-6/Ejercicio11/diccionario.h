e8d1fe5497532354a194702c0c9d8d2d6ba97dbe
Dolores Garro <129607981+doloresgarro@users.noreply.github.com>
2023-05-31 21:51:57 -0300

    Práctica 6, ej11 no andan los ultimos dos métodos

---
+++ Practica-6/Ejercicio11/diccionario.h
@@ -0,0 +1,23 @@
+#ifndef DICCIONARIO_H_INCLUDED
+#define DICCIONARIO_H_INCLUDED
+
+struct lista{
+    char dato[20];
+    struct lista *sig;
+};
+
+typedef struct lista nodo; //le mando la direccion a la direccion
+
+
+void crearDiccionario(nodo **);       //Crear un diccionario: inicializa la estructura del diccionario.
+int agregarPalabra(nodo **, char []);     //Agrega una nueva palabra al diccionario siempre y cuando no exista. Retorna si la agregó o no.
+int existePalabra(nodo *, char []);           //Existe palabra: determina si una palabra está en el diccionario o no.
+int eliminarPalabra(nodo **, char[]);         //Elimina una palabra del diccionario. Retorna si la palabra fue eliminada o no.
+void cargarDesdeArchivo(nodo *, FILE **);     //Cargar desde un archivo: carga un diccionario desde un archivo de texto.
+void guardarEnArchivo(nodo **, FILE *);  //Guarda un diccionario en un archivo de texto.
+void liberarDiccionario(nodo**); //Libera los recursos del diccionario.
+
+
+void Imprimir(nodo * l);              //lo agregue yo para ir chequeando
+
+#endif // DICCIONARIO_H_INCLUDED
