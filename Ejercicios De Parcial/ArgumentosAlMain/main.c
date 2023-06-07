28895b621239638963ac3fa511c8d7c87785f9e5
doloresgarro <doloresgarro2@gmail.com>
2023-06-07 14:13:18 -0300

    Ej argumentos al main modificado

--- Ejercicios De Parcial/ArgumentosAlMain/main.c
+++ Ejercicios De Parcial/ArgumentosAlMain/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define DIM 100
 
 int main(int argc, char * argv[])
 {
     if (!argv[1])
-        printf("Error, no se recibio ninguna palabra\n");
+        printf("Error, no se recibio ningun argumento\n");
     else {
         int i;
-        for (i = 1; i < argc; i++)
-            printf("%s ", argv[i]);
+        char resultado[DIM];
+        for (i = 1; i < argc; i++){
+            strcat(resultado, argv[i]);
+            strcat(resultado, " ");
+        }
+        printf("%s ", resultado);
     }
 
     return 0;
